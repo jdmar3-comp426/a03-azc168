6232f9a06dffb9b24f263416e5614b38edcdb46f
alan <azc@live.unc.edu>
2021-11-26 12:59:49 -0500

    test

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -62,7 +62,7 @@ export const tenTimes = multiplyBy(10);
  *   function to multiply 50 by 10 and returns the result.
  */
 export const tenTimesFifty = (tenTimes) => {
-    return 50;
+    return 50 * tenTimes;
 };
 
 
